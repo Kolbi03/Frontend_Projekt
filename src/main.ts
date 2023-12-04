8dbedaf597d6557191032775e5e94e6ac96f61b5
Petrik Diak <diak@petrik.hu>
2023-12-04 15:04:17 +0100

    event felvétel oldala

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,27 @@ import './style.css'
 import 'bootstrap/scss/bootstrap.scss';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init()
+{
+  const submit = document.getElementById('submit');
+  submit!.addEventListener('click', newEvent)
+}
+
+function newEvent()
+{
+    let title = (document.getElementById('title')as HTMLInputElement).value;
+    let description = (document.getElementById('description')as HTMLInputElement).value;
+    let date =(document.getElementById('date')as HTMLInputElement).value;
+    let iterance = (document.getElementById('iterance')as HTMLInputElement);
+
+   const events = document.getElementById('events')
+  var  newLi = document.createElement('li');
+   newLi.textContent= title+" "+description+" "+date+" "+iterance;
+   
+   
+  console.log(title);
+   events!.appendChild(newLi);
+}
