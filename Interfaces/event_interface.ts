573bd2c19e4a97e49c91bcbd8e79370c36ee20a9
Lengyel Milán <lengyel.milan.hu@gmail.com>
2023-12-15 18:29:23 +0100

    Interfaces added, smoother animations

---
+++ Interfaces/event_interface.ts
@@ -0,0 +1,9 @@
+export interface EventInterface {
+    id: number;
+    title: string;
+    description: string;
+    location: string;
+    invited: string;
+    date: Date;
+    repetition: Boolean;
+}
---
+++ Interfaces/event_interfaceDTO.ts
@@ -0,0 +1,8 @@
+export interface EventInterfaceDTO {
+    title: string;
+    description: string;
+    invited: string;
+    location: number;
+    date: Date;
+    repetiton: boolean;
+}
